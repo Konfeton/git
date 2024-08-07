3c903d0ccfc70ace13cc776e894c1f78de310707
Dzimitry <dimadulko007@mail.com>
2024-08-07 12:41:06 +0300

    feat: add and remove seats

--- Van.java
+++ Van.java
@@ -1,6 +1,14 @@
 public class Van extends Car{
     private int numberOfSeats;
 
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    public void setNumberOfSeats(int numberOfSeats) {
+        this.numberOfSeats = numberOfSeats;
+    }
+
     @Override
     public String toString() {
         return this.getClass().getSimpleName();
